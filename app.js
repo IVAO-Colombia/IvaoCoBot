6ba886837bf478223f36b6c3474b425eba526dfb
rcomunica <julir2772@gmail.com>
2024-02-23 23:39:09 -0500

    Updated 2024 and ready for published

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const wait = require('node:timers/promises').setTimeout;
-const Canvas = require('@napi-rs/canvas');
+const wait = require("node:timers/promises").setTimeout;
+const Canvas = require("@napi-rs/canvas");
 
-const { GlobalFonts } = require('@napi-rs/canvas');
-GlobalFonts.registerFromPath(path.join(__dirname, 'src', 'fonts', 'Poppins-Light.ttf'), 'Poppins Light');
-GlobalFonts.registerFromPath(path.join(__dirname, 'src', 'fonts', 'Poppins-Medium.ttf'), 'Poppins Medium');
+const { GlobalFonts } = require("@napi-rs/canvas");
+GlobalFonts.registerFromPath(
+  path.join(__dirname, "src", "fonts", "Poppins-Light.ttf"),
+  "Poppins Light"
+);
+GlobalFonts.registerFromPath(
+  path.join(__dirname, "src", "fonts", "Poppins-Medium.ttf"),
+  "Poppins Medium"
+);
 
-const { 
-  ticketsMessages,
-  lorem, 
-  errorInteraction 
-} = require('./messages.json');
-const getRoles = require('./rangos.json')
+const { ticketsMessages, lorem, errorInteraction } = require("./messages.json");
+const getRoles = require("./rangos.json");
 
 //Iniciacion de permisos y requerimientos del bot
 const {
@@ -65,7 +67,6 @@ client.once(Events.ClientReady, async (bot) => {
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-
   const command = client.commands.get(interaction.commandName);
 
   if (!command) return;
@@ -75,96 +76,91 @@ client.on(Events.InteractionCreate, async (interaction) => {
   } catch (error) {
     console.error(error);
     await interaction.reply({
-      content:
-        errorInteraction,
+      content: errorInteraction,
       ephemeral: true,
     });
   }
 });
 
-
 // Funcion de los tickets
 
-client.on( Events.InteractionCreate, async (interaction) =>{
-
-  var guild = interaction.guild
+client.on(Events.InteractionCreate, async (interaction) => {
+  var guild = interaction.guild;
   const randomValue = Math.floor(Math.random() * 9000) + 1000;
   const roles = Array.from(guild.roles.cache.values());
 
-
   async function createChannelTicket(department, roleDepartament) {
     let responseCreateEmbed = new EmbedBuilder()
-    .setColor('Random')
-    .setAuthor({
-      name: 'IVAO Colombia Tickets', 
-      iconURL: client.user.avatarURL()
-    })
-    .setURL("https://co.ivao.aero")
-    .setDescription(`
+      .setColor("Random")
+      .setAuthor({
+        name: "IVAO Colombia Tickets",
+        iconURL: client.user.avatarURL(),
+      })
+      .setURL("https://co.ivao.aero")
+      .setDescription(
+        `
       **Departamento:** ${department} 
       **Usuario:** ${interaction.member.nickname} 
       ${ticketsMessages.descriptionResponseTicket}
-    `)
-    .setFooter({ text: "Puede cerrar el ticket dandole al boton"});
+    `
+      )
+      .setFooter({ text: "Puede cerrar el ticket dandole al boton" });
 
     let closeResponseButton = new ButtonBuilder()
-    .setCustomId("closeTicket")
-    .setStyle(ButtonStyle.Danger)
-    .setLabel("❌ Cerrar")
+      .setCustomId("closeTicket")
+      .setStyle(ButtonStyle.Danger)
+      .setLabel("❌ Cerrar");
 
-    let responseRowBuilder = new ActionRowBuilder()
-      .addComponents(closeResponseButton)
+    let responseRowBuilder = new ActionRowBuilder().addComponents(
+      closeResponseButton
+    );
 
-    let staffRole = guild.roles.cache.get("1077610256844193878")
-    channelTicket = await guild.channels.create({
-      name: `ticket ${randomValue}`,
-      parent: "1077610261818658893",
-      type: ChannelType.GuildText,
-      permissionOverwrites: [
-        {
-          id: guild.roles.everyone,
-          deny: ["ViewChannel"],
-        },
-        {
-          id: interaction.user.id,
-          allow: [
-            "ViewChannel", 
-            "SendMessages", 
-            "AddReactions", 
-            "ReadMessageHistory"
-          ]
-        },
-        {
-          id: staffRole,
-          allow: [
-            "ViewChannel", 
-            "AddReactions", 
-            "ReadMessageHistory"
-          ],
-          deny: [
-            "SendMessages"
-          ]
-        },
-        {
-          id: roleDepartament,
-          allow: [
-            "ViewChannel", 
-            "SendMessages", 
-            "AddReactions", 
-            "ReadMessageHistory"
-          ]
-        },
-      ]
-    }).then(channelTicket =>{
-      channelTicket.send({
-        embeds: [responseCreateEmbed],
-        components: [responseRowBuilder] 
-      }),
-      interaction.reply({
-        content: `Su ticket se ha creado exitosamente ✅ ${channelTicket}`, 
-        ephemeral: true
+    let staffRole = guild.roles.cache.get("1077610256844193878");
+    channelTicket = await guild.channels
+      .create({
+        name: `ticket ${randomValue}`,
+        parent: "1077610261818658893",
+        type: ChannelType.GuildText,
+        permissionOverwrites: [
+          {
+            id: guild.roles.everyone,
+            deny: ["ViewChannel"],
+          },
+          {
+            id: interaction.user.id,
+            allow: [
+              "ViewChannel",
+              "SendMessages",
+              "AddReactions",
+              "ReadMessageHistory",
+            ],
+          },
+          {
+            id: staffRole,
+            allow: ["ViewChannel", "AddReactions", "ReadMessageHistory"],
+            deny: ["SendMessages"],
+          },
+          {
+            id: roleDepartament,
+            allow: [
+              "ViewChannel",
+              "SendMessages",
+              "AddReactions",
+              "ReadMessageHistory",
+            ],
+          },
+        ],
       })
-    })
+      .then((channelTicket) => {
+        channelTicket.send({
+          embeds: [responseCreateEmbed],
+          components: [responseRowBuilder],
+        }),
+          interaction.reply({
+            content: `Su ticket se ha creado exitosamente ✅ ${channelTicket}`,
+            ephemeral: true,
+          });
+      });
   }
 
   // El StringSelector son las posible selecciones del usuario
@@ -174,152 +170,166 @@ client.on( Events.InteractionCreate, async (interaction) =>{
     max_values: 1,
     options: [
       {
-        label: "Departamento de Eventos", 
-        value: "eventos", 
-        description: ticketsMessages.deployDescriptionSelection.eventsDescription
+        label: "Departamento de Eventos",
+        value: "eventos",
+        description:
+          ticketsMessages.deployDescriptionSelection.eventsDescription,
       },
       {
-        label: "Departamento de Entrenamiento / Examenes", 
-        value: "entrenamiento", 
-        description: ticketsMessages.deployDescriptionSelection.TrainingDescription
+        label: "Departamento de Entrenamiento / Examenes",
+        value: "entrenamiento",
+        description:
+          ticketsMessages.deployDescriptionSelection.TrainingDescription,
       },
       {
-        label: "Informe / Reporte de Miembros", 
-        value: "miembros", 
-        description: ticketsMessages.deployDescriptionSelection.MemebershipDescription
+        label: "Informe / Reporte de Miembros",
+        value: "miembros",
+        description:
+          ticketsMessages.deployDescriptionSelection.MemebershipDescription,
       },
       {
-        label: "Operaciones (Militares, ATC y Vuelo)", 
-        value: "operaciones", 
-        description: ticketsMessages.deployDescriptionSelection.operationDescription
+        label: "Operaciones (Militares, ATC y Vuelo)",
+        value: "operaciones",
+        description:
+          ticketsMessages.deployDescriptionSelection.operationDescription,
       },
       {
-        label: "Relaciones Publicas", 
-        value: "relaciones publicas", 
-        description: ticketsMessages.deployDescriptionSelection.publicRelationsDescription
+        label: "Relaciones Publicas",
+        value: "relaciones publicas",
+        description:
+          ticketsMessages.deployDescriptionSelection.publicRelationsDescription,
       },
       {
-        label: "Departamento web", 
-        value: "web", 
-        description: ticketsMessages.deployDescriptionSelection.webDescription
+        label: "Departamento web",
+        value: "web",
+        description: ticketsMessages.deployDescriptionSelection.webDescription,
       },
       {
-        label: "FIR(s) team", 
-        value: "fir", 
-        description: ticketsMessages.deployDescriptionSelection.firDescription
+        label: "FIR(s) team",
+        value: "fir",
+        description: ticketsMessages.deployDescriptionSelection.firDescription,
       },
-    ]
+    ],
   });
 
   // Lo convertimos a una fila
-  let firstRow = new ActionRowBuilder()
-    .addComponents(firstSelect);
+  let firstRow = new ActionRowBuilder().addComponents(firstSelect);
 
   // Instalamos y ejecutamos los tickets
 
-  if(interaction.commandName == 'install-tickets'){
+  if (interaction.commandName == "install-tickets") {
     var supportChannel = interaction.options.getChannel("canal");
     let ticketCreateEmbed = new EmbedBuilder()
-    .setColor('Random')
-    .setAuthor({name: 'IVAO Colombia Tickets', iconURL: client.user.avatarURL()})
-    .setURL("https://co.ivao.aero")
-    .setDescription(ticketsMessages.descriptionTicket)
-    .setTimestamp();
+      .setColor("Random")
+      .setAuthor({
+        name: "IVAO Colombia Tickets",
+        iconURL: client.user.avatarURL(),
+      })
+      .setURL("https://co.ivao.aero")
+      .setDescription(ticketsMessages.descriptionTicket)
+      .setTimestamp();
 
-    supportChannel.send({embeds: [ticketCreateEmbed], components: [firstRow]});
-    await interaction.reply({content: "Todo fue instalado correctamente!"})
-
+    supportChannel.send({
+      embeds: [ticketCreateEmbed],
+      components: [firstRow],
+    });
+    await interaction.reply({ content: "Todo fue instalado correctamente!" });
   }
 
-  if(interaction.customId == "selectProblem"){
+  if (interaction.customId == "selectProblem") {
     let choices = "";
 
-		await interaction.values.forEach(value =>{
-			choices += `${value}`
-		})
-    
+    await interaction.values.forEach((value) => {
+      choices += `${value}`;
+    });
+
     switch (choices) {
-      case 'eventos':
+      case "eventos":
         createChannelTicket("Eventos / Tours", getRoles[24].id);
         break;
-      case 'entrenamiento':
+      case "entrenamiento":
         createChannelTicket("Entrenamiento / Examenes", getRoles[36].id);
         break;
-      case 'miembros':
+      case "miembros":
         createChannelTicket("Informe / Reporte de Miembros", getRoles[37].id);
         break;
-      case 'operaciones': // No funciona al 100% (toca crear un unico rango para OP)
-        createChannelTicket("Operaciones (Militares, ATC y Vuelo)", getRoles[10].id);
+      case "operaciones": // No funciona al 100% (toca crear un unico rango para OP)
+        createChannelTicket(
+          "Operaciones (Militares, ATC y Vuelo)",
+          getRoles[10].id
+        );
         break;
-      case 'relaciones publicas':
+      case "relaciones publicas":
         createChannelTicket("Relaciones Publicas", getRoles[30].id);
         break;
-      case 'web':
+      case "web":
         createChannelTicket("Departamento web", getRoles[18].id);
-      break;
-      case 'fir':
+        break;
+      case "fir":
         createChannelTicket("Fir", getRoles[6].id);
-      break;
+        break;
       default:
-        interaction.reply({content: "Interaccion invalida! 📛", ephemeral: true});
+        interaction.reply({
+          content: "Interaccion invalida! 📛",
+          ephemeral: true,
+        });
         break;
     }
-
   }
 
-  if(interaction.customId == "closeTicket"){
-    let ticketChannelEnd =  interaction.guild.channels.cache.get(interaction.channelId);
+  if (interaction.customId == "closeTicket") {
+    let ticketChannelEnd = interaction.guild.channels.cache.get(
+      interaction.channelId
+    );
 
-    interaction.channel.send("Conversacion Cerrada... ¡Tenga un gran día!")
-		await wait(1500)
-		ticketChannelEnd.delete()
-			.then(deleteChannel => {
-				interaction.user.send(lorem);
-			})
-			.catch(console.error);
+    interaction.channel.send("Conversacion Cerrada... ¡Tenga un gran día!");
+    await wait(1500);
+    ticketChannelEnd
+      .delete()
+      .then((deleteChannel) => {
+        interaction.user.send(lorem);
+      })
+      .catch(console.error);
   }
-
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
+  if (interaction.commandName == "new-training") {
+    let airport = interaction.options.getString("dependencia");
+    let time = interaction.options.getString("hora");
+    let date = interaction.options.getString("fecha");
 
-    if(interaction.commandName == "new-training"){
+    try {
+      const canvas = Canvas.createCanvas(1920, 1200);
+      const context = canvas.getContext("2d");
+      const background = await Canvas.loadImage(
+        "./src/img/EntrenamientosATC.png"
+      );
 
-        let airport = interaction.options.getString("aeropuerto");
-        let time = interaction.options.getString("hora");
-        let day = interaction.options.getString("dia");
+      context.drawImage(background, 0, 0, canvas.width, canvas.height);
+      context.font = "54px Poppins Medium";
+      context.fillStyle = "#ffffff";
 
-      try {
-        const canvas = Canvas.createCanvas(1920, 1200);
-        const context = canvas.getContext('2d');
-        const background = await Canvas.loadImage('./src/img/EntrenamientosATC.png');
+      context.fillText(airport, canvas.width / 5, 650);
+      context.fillText(date, canvas.width / 5, 800);
+      context.fillText(time, canvas.width / 5, 950);
 
-        context.drawImage(background, 0,0, canvas.width, canvas.height);
-        context.font = '42px Poppins Medium';
-        context.fillStyle = '#ffffff';
+      const attachment = new AttachmentBuilder(canvas.toBuffer("image/png"), {
+        name: `anuncio-${airport}-${date}-${time}.png`,
+      });
 
-        context.fillText(airport, canvas.width / 5 , 650 );
-        context.fillText(day, canvas.width / 5 , 800);
-        context.fillText(time, canvas.width / 5 , 950 );
-        
-
-        const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'),{
-          name: 'anuncio-image.png'
-        });
-
-        
-
-        await interaction.reply({ files: [attachment] });
-      } catch (error) {
-        console.log(error);
-        await interaction.reply({
-          content: "Se ha producido un error!. Contacta a WM", ephemeral: true
-        });
-      }
-
-
+      await interaction.reply({
+        content: `**Aeropuerto:** ${airport}\n **Hora:** ${time}\n **Fecha:** ${date}\n **Requerido por:** <@${interaction.user.id}>`,
+        files: [attachment],
+      });
+    } catch (error) {
+      console.log(error);
+      await interaction.reply({
+        content: "Se ha producido un error!. Contacta a WM",
+        ephemeral: true,
+      });
     }
-
+  }
 });
 
 client.login(token);
